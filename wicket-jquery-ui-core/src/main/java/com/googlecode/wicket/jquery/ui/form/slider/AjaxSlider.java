b64884a641c63658ee7b5bf459c112583e1dbcfb
Sebastien Briquet <sebfz1@gmail.com>
2012-12-15 02:04:28 +0100

    Fixed FormSubmittingPanel Marked Ajax[Range]Slider#newOnChangeBehavior() protected Updated Homepage, download link

--- wicket-jquery-ui-core/src/main/java/com/googlecode/wicket/jquery/ui/form/slider/AjaxSlider.java
+++ wicket-jquery-ui-core/src/main/java/com/googlecode/wicket/jquery/ui/form/slider/AjaxSlider.java
@@ -143,7 +143,7 @@ public class AjaxSlider extends Slider implements IValueChangedListener
 	 * Gets a new {@link JQueryAjaxBehavior} that will be called on 'change' javascript event
 	 * @return the {@link JQueryAjaxBehavior}
 	 */
-	private JQueryAjaxBehavior newOnChangeBehavior()
+	protected JQueryAjaxBehavior newOnChangeBehavior()
 	{
 		return new JQueryAjaxChangeBehavior(this, this.input) {
 
