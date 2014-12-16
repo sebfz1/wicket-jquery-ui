458e722e3af75d901ce8219bdf8669317424c445
Sebastien Briquet <sebastien.briquet@amadeus.com>
2014-12-16 17:48:25 +0100

    KendoUI: Accordion, added ExpandBehavior & CollapseBehavior

---
+++ wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/widget/accordion/ExpandBehavior.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.wicket.kendo.ui.widget.accordion;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.util.lang.Args;
+
+import com.googlecode.wicket.jquery.core.IJQueryWidget.JQueryWidget;
+import com.googlecode.wicket.jquery.core.JQueryBehavior;
+
+/**
+ * Provides the ability to expands panes
+ *
+ * @author Sebastien Briquet - sebfz1
+ * @since 6.19.0
+ * @since 7.0.0
+ */
+public class ExpandBehavior extends JQueryBehavior
+{
+	private static final long serialVersionUID = 1L;
+
+	// TODO: move to AccordionBehavior (when will exists)
+	static final String METHOD = "kendoPanelBar";
+	
+	/** children selector TODO: to be supplied through ctors */
+	private static final String CHILDREN_ALL = "li";
+
+	/**
+	 * Constructor
+	 */
+	public ExpandBehavior()
+	{
+		super(null);
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @param selector the html selector (ie: "#myId")
+	 */
+	public ExpandBehavior(String selector)
+	{
+		super(selector);
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @param component the {@link Component}
+	 */
+	public ExpandBehavior(Component component)
+	{
+		super(JQueryWidget.getSelector(component));
+	}
+
+	// Properties //
+
+	@Override
+	public boolean isTemporary(Component component)
+	{
+		return true;
+	}
+
+	// Methods //
+
+	@Override
+	public void bind(Component component)
+	{
+		super.bind(component);
+
+		if (this.selector == null)
+		{
+			this.selector = JQueryWidget.getSelector(component);
+		}
+	}
+
+	private String widget()
+	{
+		return String.format("jQuery('%s').data('%s')", Args.notNull(this.selector, "selector"), METHOD);
+	}
+
+	@Override
+	public String $()
+	{
+		return String.format("%s.expand(jQuery('%s'), false);", this.widget(), CHILDREN_ALL);
+	}
+}
