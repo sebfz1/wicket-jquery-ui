b2dca2db52026a654cdc8885fd01b22b2d9d4828
Tedlabs46 <steven.baldet@amadeus.com>
2015-08-18 13:34:03 +0200

    .:::::::::.                                   .:::::::::::::::. 				 ::::::::::::::::::: 				:::::::::::::::::::::                              :  `:::::::::::::::::::::                             :: >. `:::::::::::::::'''  .:: 			    :: `{C>,..````````... 			    .::''`:::..``{{{CCC>>>''..:::''.                    .,c$$h.    `'::::::.....:::''''.,c$$L                   J$$$$$.?. ,d$=  `````.,   .zcc$$$$$$$$.                 ,$$$$$$$F."4P".:`:: ,$$".:::. "$$$$$$$$$h   ,cc,                ,$$$$$P".,P." '',c `,d$$ `'``':.`$$$$$$$$$h ="3$$c. 	      .$$$$$F $$" dc -?""? ?$$',c$$c  ` ?$$$$$$$$F, d$$$$h. 	      J$$$$$$ $F ,$',""$$h.?$',,,,."" . `$$$$$$$$ d 3$$$$$$. 	      3$$$$$P $F $$    ?$$'J',$" `$$c  $$$$$$$$$P $h "$$$$$$ 	       ?C'.,c$P $$$    J$$ J $F   $$$h $$$$$$$$$' $$$ $$$$$$h 	        `??""  ,$$$ h,c$$F.$ $h, ,$$$',$$$$$$$$$c,`$P.$$$$$$$                       ,$$$$c`??"",c, "$$$$$P',$$$$$$$$$$$h ?L'$$$$$$P 		      $$$$$$$F zd$$$h  """',d$$$$$$$$$$$$$h`?.`??$$P' 		     J$$$$P",c$$$$$$$h."$$$$$$$$$$$$$$$$$$$h. `??"'      ,cd$$$$$$$$$$h J$$$$",J$$$$$$$$$$$c ?$$$$$$$$$$$$$$$$$$$    ,$$???????$$$$$F.$$$$$ $$$$$$$$$$$$$$h`$$$$$$$$$$$$$$$$$$$h   J$P"        `""" J$$$$$c`$$$$$$$$$$$$$F,$$$$$$$$$$$$$$$$$P"'  d$"               J$??".  `??$$$$?????" $$$$$$$$$$$$$$$$$$ J$$$$$hcc,. J$$                J" ,','  ;    , ,. . . `"?$$$$$$$$$$$$$$ $$$$$$$$$$$$c, 3$$                J ' > .{! ;! ;!;'!> - `!.  "?$$$$$$$$$$$.'$P??""""""?$$h `$P",nmn        .   c ' ;!! ;!! !!! '!> !; {!.`;`$$$???$$$$$c,           ?$$  ? uMMP ,nmnmdMMM'  3$c, `  ''' {!!''!!> !> `' ,c$$F nx.?$$$$$            $$   dMM",dMMMMMMP"'   `$$$h.`  ,____,=  `. ' ,ccc$$$$h MMMx."??"           z$$   MP nMMMMMMMM,      ?$$$$.`\ """"" ,-' .d$$$$$$$$$$c )MMMMMn.  ..     ,c$$P   " =MMMMMMMMMMMbm,   ?$$$$h.``````',c$$$$$$$$?????"",dMMMMMMMb;MMr  .z$$$P"      "MMMMMMMMMMMMP'   ?$$$$$$$$$$$$$$$$$$$$$ nmMMMMMMMMMMMMMMP{MMM ?$$P"       `MMMMMMM          "$$$$$$$$$$$$$$$$$$$$."4MMMMMMMMMMMMMMb`MMM.`"        MM4MMMMx           "??$$$$$$$$$$$$$$$$$ccc= ,MMMMMMMMMMM "MM        4Mr "4MMr               ""?????????????"",nmMMMMMMMMMMMMP  "        `M    `"                $|c             4MMMPPP4MMMMMMP                                $|$              "      MMMM" 			       $|$              ;!;   dMMP'                            ,,, "|"              ;' {! P" ;{!                       . . .  ...  -          `" , ;,,. -'!!!.                      ''' '.;;;;;.         ,!' ." `-._``--..`'                     ;;;;;..```..;;;{>   ''- ...;;;.   `~- ' ;; 		  .`''!!!!!!!!''''`.;; ;;;;;;..````''-;' ,;!!' 		  ''{;;......;;;;-'`   `'!!!!!!!!!!-- ,;!!'`                        ```````        !!;,,.````.,,;{!!'`                                        ```''''''''''``

--- wicket-jquery-ui-samples/src/main/java/com/googlecode/wicket/jquery/ui/samples/pages/kendo/radio/DefaultRadioPage.java
+++ wicket-jquery-ui-samples/src/main/java/com/googlecode/wicket/jquery/ui/samples/pages/kendo/radio/DefaultRadioPage.java
@@ -1,10 +1,61 @@
 package com.googlecode.wicket.jquery.ui.samples.pages.kendo.radio;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RadioGroup;
+
+import com.googlecode.wicket.kendo.ui.form.Radio;
+import com.googlecode.wicket.kendo.ui.form.Radio.Label;
+import com.googlecode.wicket.kendo.ui.form.button.AjaxButton;
+import com.googlecode.wicket.kendo.ui.panel.KendoFeedbackPanel;
+
 public class DefaultRadioPage extends AbstractRadioPage
 {
     private static final long serialVersionUID = 1L;
 
     public DefaultRadioPage(){
         
+        // Radio //
+        RadioGroup<String> group = new RadioGroup<String>("radiogroup");
+        this.add(group);
+
+        Radio<String> radio1 = new Radio<String>("radio1", group);
+        group.add(radio1);
+        Label label1 = new Label("label1", "Radio1", radio1);
+        group.add(label1);
+
+        Radio<String> radio11 = new Radio<String>("radio11", group);
+        group.add(radio11);
+        Label label11 = new Label("label11", "Radio11", radio11);
+        group.add(label11);
+
+        Radio<String> radio2 = new Radio<String>("radio2", group);
+        group.add(radio2.setEnabled(false));
+
+        Label label2 = new Label("label2", "Radio2", radio2);
+        group.add(label2);
+
+        // Feedback //
+        final KendoFeedbackPanel feedback = new KendoFeedbackPanel("feedback");
+        this.add(feedback);
+
+        // Form //
+        final Form<Void> form = new Form<Void>("form");
+        this.add(form);
+
+        // Buttons //
+        form.add(new AjaxButton("show") {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+
+                this.info("Simple message");
+
+                target.add(feedback);
+            }
+        });
+
     }
 }
