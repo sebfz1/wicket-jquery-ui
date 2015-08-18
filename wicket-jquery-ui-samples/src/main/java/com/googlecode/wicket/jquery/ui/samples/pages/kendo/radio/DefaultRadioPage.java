e23535dd1d413568000fcf093bab3a6011dbef68
Tedlabs46 <steven.baldet@amadeus.com>
2015-08-18 09:28:31 +0200

    Create test page and move to the right package

---
+++ wicket-jquery-ui-samples/src/main/java/com/googlecode/wicket/jquery/ui/samples/pages/kendo/radio/DefaultRadioPage.java
@@ -0,0 +1,10 @@
+package com.googlecode.wicket.jquery.ui.samples.pages.kendo.radio;
+
+public class DefaultRadioPage extends AbstractRadioPage
+{
+    private static final long serialVersionUID = 1L;
+
+    public DefaultRadioPage(){
+        
+    }
+}
