5a20bc35d7b42ee599d9e74da1e24711d67b7e61
Sebastien Briquet <sbriquet@apache.org>
2016-07-09 14:01:06 +0200

    jQuery UI: TabbedPanel, added #newTabContainer; fixes #236

--- wicket-jquery-ui-samples/src/main/java/com/googlecode/wicket/jquery/ui/samples/jqueryui/tabs/AdvancedTabsPage.java
+++ wicket-jquery-ui-samples/src/main/java/com/googlecode/wicket/jquery/ui/samples/jqueryui/tabs/AdvancedTabsPage.java
@@ -3,6 +3,7 @@ package com.googlecode.wicket.jquery.ui.samples.jqueryui.tabs;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.core.request.handler.IPartialPageRequestHandler;
 import org.apache.wicket.event.Broadcast;
@@ -10,9 +11,12 @@ import org.apache.wicket.event.IEvent;
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.link.AbstractLink;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.slf4j.Logger;
@@ -66,6 +70,39 @@ public class AdvancedTabsPage extends AbstractTabsPage
 				this.info("selected tab #" + index);
 
 				target.add(feedback);
+			}
+
+			@Override
+			protected WebMarkupContainer newTabContainer(String id, String tabId, ITab tab, int index)
+			{
+				Panel panel = new TabPanel(id);
+
+				Label label = new Label("link", tab.getTitle());
+				label.add(AttributeModifier.replace("href", "#" + tabId));
+				panel.add(label);
+
+				AbstractLink link = this.newRemoveLink("remove", index);
+				panel.add(link);
+
+				return panel;
+			}
+
+			private AbstractLink newRemoveLink(String id, final int index)
+			{
+				return new org.apache.wicket.ajax.markup.html.AjaxLink<Integer>(id, Model.of(index)) {
+
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public void onClick(AjaxRequestTarget target)
+					{
+						tabPanel.getModelObject().remove(index);
+						target.add(tabPanel.setActiveTab(0));
+
+						// refreshes the navigation buttons //
+						this.send(buttons, Broadcast.EXACT, new ChangeEvent(0, target));
+					}
+				};
 			}
 		};
 
@@ -297,6 +334,16 @@ public class AdvancedTabsPage extends AbstractTabsPage
 		public AjaxRequestTarget getTarget()
 		{
 			return this.target;
+		}
+	}
+
+	static class TabPanel extends Panel
+	{
+		private static final long serialVersionUID = 1L;
+
+		public TabPanel(String id)
+		{
+			super(id);
 		}
 	}
 }
